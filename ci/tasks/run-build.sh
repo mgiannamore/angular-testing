c31066625f0e678e4672c67baeeea249d3f453f3
Marcus Giannamore <marcus.giannamore@dcsg.com>
2018-09-17 16:34:52 -0400

    checking to see where the fuck i am

--- ci/tasks/run-build.sh
+++ ci/tasks/run-build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-cd codebase
-npm install
-ng build --prod --aot
-cp -r ./dist/* ../compiled-code/
-cd ../compiled-code
 pwd
 ls
+
+#npm install
+#ng build --prod --aot
+#cp -r ./dist/* ../compiled-code/
+#cd ../compiled-code
+#pwd
+#ls
